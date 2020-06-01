e01af04f5b9e17e53b674b56638884078976f584
Sumedh Patil <sumedhpatil675@gmail.com>
2020-06-01 13:00:25 +0530

    Add files via upload

--- task_by_aman_chauhan_sir.py
+++ task_by_aman_chauhan_sir.py
@@ -9,13 +9,14 @@ from sklearn.datasets import fetch_openml
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 
 from sklearn.tree import DecisionTreeClassifier
-
+import seaborn as sns
 
 
 
@@ -60,30 +61,138 @@ accuracy = accuracy.mean()
 accuracy
  
 
-#########################################################
+##############################################################################
 
 
 
-##DecisionTree Classifier
-########################################################
-#making instance of model
-dt = DecisionTreeClassifier()
-
-dt.fit(x_train,y_train)
-
-y_pred=dt.predict(x_test)
-
-
-count =0 
-for i in range(0,6000):
-    count+=1 if y_pred[i] == y_test[i] else 0
-
-
-count
+##SVM Classifier
+##############################################################################
+#importinv neccesary libraries
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.model_selection import validation_curve
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
 
 
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
 
+#printing shape of the data
+train_data.shape
+
+#checking for missing values
+train_data.isnull().sum()
+
+#seeing description of test data
+test_data.describe()
+
+#checking dimensions of dataset
+print("Dimensions : ",test_data.shape, "\n")
+
+#datatypes
+test_data.info()
+
+
+#checking columns of dataset
+train_data.columns
+
+#checking unique values in label columns(classes)
+order = list(np.sort(train_data['label'].unique()))
+print(order)
+
+#visualizing number of class and counts in datasets
+plt.plot(figure=(16,10))
+g = sns.countplot(train_data['label'],palette='icefire')
+plt.title('Number of digit classes')
+train_data.label.astype('category').value_counts()
+
+#ploting some sample and converting into matrix
+four = train_data.iloc[3,1:]
+four.shape
+four = four.values.reshape(28,28)
+plt.imshow(four,cmap='gray')
+plt.title("Digit 4") 
+
+ #Data Preparation#
+ 
+#average feature values
+round(train_data.drop('label',axis=1).mean(),2)
+
+#Seperating X and Y variables
+ 
+#labels
+y = train_data['label']
+
+#droping label from X var
+X = train_data.drop(columns='label')
+
+##Normalization
+X = X/255.0
+test_data = test_data/255.0
+
+
+#Scaling the features
+from sklearn.preprocessing import scale 
+X_scaled = scale(X)
+
+#spliting data to train model
+X_train,X_test,y_train,y_test = train_test_split(X_scaled,y,test_size=0.3,train_size=0.2,random_state=10)
+
+
+#Model building
+
+model_linear = SVC(kernel='linear')
+model_linear.fit(X_train,y_train)
+
+#predict
+y_pred = model_linear.predict(X_test)
+
+
+#checking confusion matrix and accuracyy
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
+#accuracy
+print("accuracy :", metrics.accuracy_score(y_true=y_test,y_pred=y_pred),"\n")   
+
+#cm
+print(metrics.confusion_matrix(y_true=y_test,y_pred=y_pred))
+
+###############################################################################
+
+
+
+####KNN--Classifier--#########################################################
+
+
+#applying PCA to the data
+from sklearn.decomposition import PCA
+pca = PCA(.95)
+pca.fit(X_train)
+X_train = pca.transform(X_train)
+X_test = pca.transform(X_test)
+
+#loading knn model 
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=7,n_jobs=-1)
+
+#training the model
+knn.fit(X_train,y_train)
+
+#making prediction
+y_pred2 = knn.predict(X_test)
+
+print("accuracy :", metrics.accuracy_score(y_true=y_test,y_pred=y_pred2),"\n")   
+
+
+################################################################333
 
 
 
